83fad67c426fcc2f0432c22df6332a966fd77c57
Mike Moore <tigerhollowpoint@gmail.com>
2022-04-28 09:29:34 -0400

    Update index.tsx

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,16 +2,17 @@ import React from "react";
 import styles from "./styles.module.scss"
 interface ButtonProps {
     text: string,
-    action: Function,
-    disabled?:boolean
+    action?: Function,
+    disabled?:boolean,
+    type? : string
 }
-const Button = ({text,action,disabled}:ButtonProps) => {
+const Button = ({type,text,action,disabled}:ButtonProps) => {
     const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
         action(); 
     }
 
-    return <button disabled={disabled} className={styles.button} onClick={handleClick}>{text}</button>
+    return <button type={type||"button"} disabled={disabled} className={styles.button} onClick={action?handleClick||undefined}>{text}</button>
 }
 
 export default Button ; 
